f3dc39315668038ad8ba23bd81698d714aa0d633
Matthias <maciej.plis.secure@gmail.com>
2020-07-05 12:26:48 +0200

    initial commit

---
+++ src/test/java/matthias/tictactoe/TicTacToeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package matthias.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
