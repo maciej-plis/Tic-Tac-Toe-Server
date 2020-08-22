93511e539f1fb79fdce7593a64a73479555049c9
Matthias <maciej.plis.secure@gmail.com>
2020-08-22 23:12:19 +0200

    Creating 2 users for testing when starting program

--- src/main/java/matthias/tictactoe/TicTacToeServerApplication.java
+++ src/main/java/matthias/tictactoe/TicTacToeServerApplication.java
@@ -1,15 +1,45 @@
 package matthias.tictactoe;
 
+import lombok.RequiredArgsConstructor;
+import matthias.tictactoe.web.authentication.model.Role;
+import matthias.tictactoe.web.authentication.model.User;
+import matthias.tictactoe.web.authentication.services.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@RequiredArgsConstructor
 @SpringBootApplication
-public class TicTacToeServerApplication {
+public class TicTacToeServerApplication implements CommandLineRunner {
+    private final UserService userService;
 
     public static void main(String[] args) {
         SpringApplication.run(TicTacToeServerApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Set<Role> roles = new HashSet<>();
+        roles.add(Role.USER);
 
+        User user1 = User.builder()
+                        .username("RICO5k")
+                        .email("maciekqwertyzaqwsx@gmail.com")
+                        .password("$2a$10$q5pIy.6HnZyj8bmk6gUr8umQodqsd1PUaJvg4IcEQa.BMRlyZV196")
+                        .roles(roles)
+                        .build();
+        userService.saveUser(user1);
+
+        User user2 = User.builder()
+                .username("alfa1")
+                .email("alfa1@gmail.com")
+                .password("$2a$10$TI/v1CU.RCswJoQJMbLD5usP6aSoNigYJWRS01RNxSGyDDIEKo/wm")
+                .roles(roles)
+                .build();
+        userService.saveUser(user2);
+    }
 }
 
